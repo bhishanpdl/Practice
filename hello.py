961ed011008f7124e1268b9ad4d14756981c52de
Your Name <you@example.com>
2016-08-24 17:50:38 -0400

    added hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author      : Bhishan Poudel; Physics Graduate Student, Ohio University
+# Date        : Aug 24, 2016
+# Last update : 
+#
+# Depends     : none
+#
+# Outputs     : 
+#
+# Info:
+# 1. 
+#
+
+print("This is hello world program.")
