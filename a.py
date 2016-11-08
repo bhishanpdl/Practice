4a3fb0dee9521b6f6be0ff1f80459338bf8e6755
bhishanpdl <bhishanpdl@gmail.com>
2016-11-08 13:51:36 -0500

    edited a.py

---
+++ a.py
@@ -0,0 +1,9 @@
+# @Author: Bhishan Poudel <poudel>
+# @Date:   24-Aug-2016 17:08
+# @Last modified by:   poudel
+# @Last modified time: 08-Nov-2016 13:11
+
+a = 5
+b = 6
+
+print('{} {:.3g} {}'.format('b', b, ''))
