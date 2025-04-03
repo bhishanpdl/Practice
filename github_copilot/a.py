5e418d680e19c38471e909e35000ac53973286fa
bhishanpdl <bhishantryphysics@gmail.com>
2025-04-03 10:03:41 -0400

    vscode fib lru cache

--- github_copilot/a.py
+++ github_copilot/a.py
@@ -1,13 +1,15 @@
+from functools import lru_cache
 
+@lru_cache(maxsize=None)
+def fibonacci_number(n):
+    """Return the nth Fibonacci number."""
+    if n < 2:
+        return n
+    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
 
 def fibonacci_series(n):
-    """Generate a Fibonacci series up to n terms."""
-    series = []
-    a, b = 0, 1
-    for _ in range(n):
-        series.append(a)
-        a, b = b, a + b
-    return series
+    """Generate a Fibonacci series up to n terms using lru_cache."""
+    return [fibonacci_number(i) for i in range(n)]
 
 # Example usage:
 print(fibonacci_series(10))  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
